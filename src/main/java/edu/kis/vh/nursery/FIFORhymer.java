90c33db9e62a92fcac71d715f78a9cdfe64259e3
Maciej <255680@edu.p.lodz.pl>
2026-03-12 14:59:42 +0100

    4.1 poprawa nazw klas

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer buffer = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer buffer = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
