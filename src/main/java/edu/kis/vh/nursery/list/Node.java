93f12e3d79f3b7e3daecc77af56d97487f8a10c8
Maciej Firlej <255583@edu.p.lodz.pl>
2026-03-26 14:55:58 +0100

    11. Hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -5,6 +5,30 @@ public class Node {
     private int value;
     private Node prev, next;
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
     public Node(int i) {
         value = i;
     }
